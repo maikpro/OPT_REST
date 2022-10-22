1b1bfe3f0182286252c8c899f99f2fb7df32206d
Maik Proba <maik.proba@gmail.com>
2022-10-22 18:15:51 +0200

    fixed bug: some filler was not detected.

--- rest-client/optRestClient.js
+++ rest-client/optRestClient.js
@@ -2,49 +2,51 @@ const axios = require('axios').default;
 const cheerio = require('cheerio');
 
 async function getFolgen() {
-    return await axios.get('https://onepiece-tube.com/episoden-streams').then((response) => {
-        // get Website data
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const episodes = $('.mediaitem');
-        const folgenList = [];
+  return await axios
+    .get('https://onepiece-tube.com/episoden-streams')
+    .then((response) => {
+      // get Website data
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const episodes = $('.mediaitem');
+      const folgenList = [];
 
-        episodes.each(function (index) {
-            const folgeName = $(this).text().split("\n")[2].trim();
-            let isFiller = false;
-            let isNew = false;
+      episodes.each(function (index) {
+        const nr = index + 1;
+        const folgeName = $(this).text().split('\n')[2].trim();
+        let isFiller = false;
+        let isNew = false;
 
-            // Fillerfolge markieren
-            const childNodes = $(this).children('td').eq(1)[0].childNodes
-            if (childNodes.length === 2) {
-                const fillerMarker = childNodes[1].attribs.src;
+        // Fillerfolge markieren
+        const childNodes = $(this).children('td').eq(1)[0].childNodes;
+        const fillerMarker = childNodes[1]
+          ? childNodes[1].attribs.src
+          : undefined;
 
-                if (fillerMarker.includes("filler.gif")) {
-                    isFiller = true;
-                }
+        if (fillerMarker && fillerMarker.includes('filler.gif')) {
+          isFiller = true;
+        }
 
-                if (fillerMarker.includes("new.gif")) {
-                    isNew = true;
-                }
-            }
+        if (fillerMarker && fillerMarker.includes('new.gif')) {
+          isNew = true;
+        }
 
-            const nr = index + 1;
-            const link = 'https://onepiece-tube.com/folge/' + nr;
-            const folgeObj = {
-                nr,
-                name: folgeName,
-                isFiller,
-                isNew,
-                link
-            }
-            folgenList.push(folgeObj);
-        });
+        const link = 'https://onepiece-tube.com/folge/' + nr;
+        const folgeObj = {
+          nr,
+          name: folgeName,
+          isFiller,
+          isNew,
+          link,
+        };
+        folgenList.push(folgeObj);
+      });
 
-        return folgenList;
-
-    }).catch((error) => {
-        // handle error
-        console.log(error);
+      return folgenList;
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
     });
 }
 
